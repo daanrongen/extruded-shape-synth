146db7a35d8764d8740e691305e138a05313819b
daanrongen <daanrongen@hotmail.com>
2020-12-05 20:06:43 +0000

    cleanup|

--- week-6/script.js
+++ week-6/script.js
@@ -1,10 +1,6 @@
 function main() {
   const canvas = document.querySelector("canvas")
-
   const renderer = new THREE.WebGLRenderer({ canvas })
-  const loader = new THREE.TextureLoader()
-
-  renderer.setClearColor(0xffffff, 1)
 
   const camera = new THREE.PerspectiveCamera(
     40,
@@ -12,46 +8,39 @@ function main() {
     0.1,
     1000
   )
-  camera.position.set(0, 40, 0)
+  camera.position.set(0, 5, 0)
   camera.up.set(0, 0, 1)
   camera.lookAt(0, 0, 0)
 
   const scene = new THREE.Scene()
 
   const light = new THREE.PointLight(0xffffff, 1)
-  light.position.set(100, 100, -100)
+  light.position.set(-10, 10, 10)
   scene.add(light)
-
-  var geometry = new THREE.SphereGeometry(1, 32, 32)
 
   const objects = []
 
-  const earthOrbit = new THREE.Object3D()
-  scene.add(earthOrbit)
-  objects.push(earthOrbit)
-
-  loader.load("2_no_clouds_4k.jpg", (texture) => {
-    const earthMaterial = new THREE.MeshPhongMaterial({
+  const earthMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 32, 32),
+    new THREE.MeshPhongMaterial({
       color: 0x2233ff,
       emissive: 0x112244,
-      map: texture,
     })
-
-    const earthMesh = new THREE.Mesh(geometry, earthMaterial)
-    earthMesh.scale.set(5, 5, 5)
-    earthOrbit.add(earthMesh)
-    objects.push(earthMesh)
-  })
+  )
+  scene.add(earthMesh)
+  objects.push(earthMesh)
 
   const moonOrbit = new THREE.Object3D()
-  moonOrbit.position.x = 10
-  earthOrbit.add(moonOrbit)
+  moonOrbit.position.x = 1.4
+  earthMesh.add(moonOrbit)
 
-  const moonMaterial = new THREE.MeshPhongMaterial({
-    color: 0x888888,
-    emissive: 0x222222,
-  })
-  const moonMesh = new THREE.Mesh(geometry, moonMaterial)
+  const moonMesh = new THREE.Mesh(
+    new THREE.SphereGeometry(0.1, 32, 32),
+    new THREE.MeshPhongMaterial({
+      color: 0x888888,
+      emissive: 0x222222,
+    })
+  )
   moonOrbit.add(moonMesh)
   objects.push(moonMesh)
 
