3e5718eac0fc72c0ab6c406a3596c98586d049fa
daanrongen <daanrongen@hotmail.com>
2021-01-13 18:56:17 +0000

    draw circles on mousepos

--- final-project/script.js
+++ final-project/script.js
@@ -12,84 +12,89 @@
  * https://jsfiddle.net/wilt/a21ey9y6/
  */
 
-var canvas, renderer, camera, light, scene
+var canvas, renderer, camera, light, scene, controls
+var view
 var width = window.innerWidth
 var height = window.innerHeight
 
 var mouse = new THREE.Vector3()
-var mouseDown = false
+var posArray = []
+var pos1, pos2
+
+var circles = []
 
 var line
 var drawCount
-var posArray = []
 
 function main() {
   canvas = document.querySelector("canvas")
-  //   canvas.addEventListener("mousemove", onMouseMove, false)
   canvas.addEventListener("mousedown", onMouseDown, false)
-  //   canvas.addEventListener("mouseup", onMouseUp, false)
   canvas.width = width
   canvas.height = height
 
   renderer = new THREE.WebGLRenderer({ canvas, alpha: true })
   scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 100)
+  camera = new THREE.PerspectiveCamera(50, width / height, 2, 100)
   camera.position.set(0, 0, 5)
   camera.up.set(0, 0, 1)
   camera.lookAt(0, 0, 0)
+
+  //   controls = new THREE.OrbitControls(camera, renderer.domElement)
 
   light = new THREE.PointLight(0xffffff, 1)
   light.position.set(10, 10, 10)
   scene.add(light)
 
-  //   const customPts = []
-  //   customPts.push(new THREE.Vector3(0, 0, 0.3))
-  //   customPts.push(new THREE.Vector3(0.2, 0.3, 0.3))
-  //   customPts.push(new THREE.Vector3(0, 0.5, 0.5))
-
-  //   const customShape = new THREE.Shape(customPts)
-  //   addShape(customShape, 0x0000ff, 0, 0, 0, 1)
-  //   addLine(customShape, 0x0000ff, 0, 0, 0, 1)
-
   var geometry = new THREE.BufferGeometry()
-  var positions = new Float32Array(500 * 3)
+  var positions = new Float32Array(2 * 3)
   addLine(geometry, positions, 0x0000ff)
-
   updatePositions()
+
   requestAnimationFrame(render)
 }
 
-render = (time) => {
+render = () => {
   renderer.render(scene, camera)
 
   drawCount = posArray.length
   line.geometry.setDrawRange(0, drawCount)
   line.geometry.attributes.position.needsUpdate = true
-  updatePositions()
 
+  updatePositions()
   requestAnimationFrame(render)
 }
 
+drawCircle = ({ x, y, z }, r, s) => {
+  console.log(x, y, z)
+
+  var material = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+    wireframe: true,
+  })
+
+  var circleGeometry = new THREE.CircleGeometry(r, s)
+  var circle = new THREE.Mesh(circleGeometry, material)
+  circle.position.set(x, y, z)
+  circle.rotation.set(0, 5, 0)
+
+  scene.add(circle)
+}
+
 addLine = (geometry, positions, color) => {
-  geometry.addAttribute("position", new THREE.BufferAttribute(positions, 3))
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
   drawCount = 2
   geometry.setDrawRange(0, drawCount)
 
   var material = new THREE.LineBasicMaterial({ color })
   line = new THREE.Line(geometry, material)
+
   scene.add(line)
-
-  addShape(geometry, positions, color)
-}
-
-addShape = (geometry, positions, color) => {
-  const shape = new THREE.Shape()
 }
 
 updatePositions = () => {
-  var positions = line.geometry.attributes.position.array
-  var index = 0
+  let positions = line.geometry.attributes.position.array
+  let index = 0
 
   for (let i = 0; i < posArray.length; i++) {
     positions[index++] = posArray[i].x
@@ -102,7 +107,7 @@ onMouseMove = (event) => {
   if (renderer) {
     mouse.x = (event.clientX / width) * 2 - 1
     mouse.y = -(event.clientY / height) * 2 + 1
-    var currentPos = new THREE.Vector3(mouse.x, mouse.y, 0)
+    let currentPos = new THREE.Vector3(mouse.x, mouse.y, 0)
 
     currentPos.unproject(camera)
     posArray.push(currentPos)
@@ -110,19 +115,22 @@ onMouseMove = (event) => {
 }
 
 onMouseDown = (event) => {
-  var x = (event.clientX / width) * 2 - 1
-  var y = -(event.clientY / height) * 2 + 1
+  let x = (event.clientX / width) * 2 - 1
+  let y = -(event.clientY / height) * 2 + 1
+  let z = 0
 
-  var currentPos = new THREE.Vector3(x, y, 0)
+  let currentPos = new THREE.Vector3(x, y, z)
   currentPos.unproject(camera)
   posArray.push(currentPos)
 
-  document.addEventListener("mousemove", onMouseMove, false)
-  document.addEventListener("mouseup", onMouseUp, false)
+  drawCircle(currentPos, 0.5, 8)
+
+  canvas.addEventListener("mousemove", onMouseMove, false)
+  canvas.addEventListener("mouseup", onMouseUp, false)
 }
 
 onMouseUp = (event) => {
-  document.removeEventListener("mousemove", onMouseMove, false)
+  canvas.removeEventListener("mousemove", onMouseMove, false)
 }
 
 main()
