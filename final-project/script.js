25726697a1a56a98a462f20fbea86fe442b91e13
daanrongen <daanrongen@hotmail.com>
2021-01-14 18:07:56 +0000

    get vertices of circles

--- final-project/script.js
+++ final-project/script.js
@@ -12,19 +12,18 @@
  * https://jsfiddle.net/wilt/a21ey9y6/
  */
 
-var canvas, renderer, camera, light, scene, controls
-var view
+var canvas, renderer, camera, light, scene, group
 var width = window.innerWidth
 var height = window.innerHeight
 
-var mouse = new THREE.Vector3()
-var posArray = []
-var pos1, pos2
-
+var circle
 var circles = []
 
+var mouse = new THREE.Vector3()
+var drawCount = 2
+var posArray = []
+
 var line
-var drawCount
 
 function main() {
   canvas = document.querySelector("canvas")
@@ -33,23 +32,23 @@ function main() {
   canvas.height = height
 
   renderer = new THREE.WebGLRenderer({ canvas, alpha: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
   scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera(50, width / height, 1, 100)
+  camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 100)
   camera.position.set(0, 0, 5)
-  camera.up.set(0, 0, 1)
+  camera.up.set(0, 1, 0)
   camera.lookAt(0, 0, 0)
 
-  //   controls = new THREE.OrbitControls(camera, renderer.domElement)
-
   light = new THREE.PointLight(0xffffff, 1)
-  light.position.set(10, 10, 10)
+  light.position.set(5, 5, 5)
   scene.add(light)
 
-  var geometry = new THREE.BufferGeometry()
-  var positions = new Float32Array(2 * 3)
-  addLine(geometry, positions, 0x0000ff)
-  updatePositions()
+  group = new THREE.Group()
+  scene.add(group)
+
+  addLine(0x0000ff)
 
   requestAnimationFrame(render)
 }
@@ -65,59 +64,64 @@ render = () => {
   requestAnimationFrame(render)
 }
 
-drawCircle = ({ x, y, z }, r, s) => {
-  let material = new THREE.MeshBasicMaterial({
-    color: 0x0000ff,
-    wireframe: true,
-  })
+addShape = (shape, color, { x, y, z }, { rx, ry, rz }) => {
+  const geometry = new THREE.ShapeBufferGeometry(shape)
 
-  let circleGeometry = new THREE.CircleGeometry(r, s)
-  let circle = new THREE.Mesh(circleGeometry, material)
-  circle.position.set(x, y, z)
-  circle.rotation.set(0, 5, 0)
+  mesh = new THREE.Mesh(
+    geometry,
+    new THREE.MeshPhongMaterial({ color: color, wireframe: true })
+  )
+  mesh.position.set(x, y, z)
+  mesh.rotation.set(rx, ry, rz)
+  group.add(mesh)
 
-  circles.push({ circleGeometry, x, y, z })
-  scene.add(circle)
+  addLineShape(shape, color, { x, y, z }, { rx, ry, rz })
+}
+
+addLineShape = (shape, color, { x, y, z }, { rx, ry, rz }) => {
+  const points = shape.getPoints()
+
+  const geometryPoints = new THREE.BufferGeometry().setFromPoints(points)
+  const line = new THREE.Line(
+    geometryPoints,
+    new THREE.LineBasicMaterial({ color: color })
+  )
+  line.position.set(x, y, z)
+  line.rotation.set(rx, ry, rz)
+
+  const particles = new THREE.Points(
+    geometryPoints,
+    new THREE.PointsMaterial({ color: color, size: 0.05 })
+  )
+  particles.position.set(x, y, z)
+  particles.rotation.set(rx, ry, rz)
+
+  circles.push({ points, x, y, z })
+  group.add(line, particles)
+}
+
+addLine = (color) => {
+  const geometry = new THREE.BufferGeometry()
+  let positions = new Float32Array(drawCount * 3)
+
+  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
+  geometry.setDrawRange(0, drawCount)
+
+  const material = new THREE.LineBasicMaterial({ color, linewidth: 1 })
+  line = new THREE.Line(geometry, material)
+
+  group.add(line)
 }
 
 drawCylinder = (c1, c2) => {
-  //   console.log(c1, c2)
-  let material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-  })
+  console.log(c1, c2)
+  let vertices = []
 
-  let geometry = new THREE.Geometry()
+  //   c1.points.forEach((v2) => vertices.push(new THREE.Vector3(v2.x, v2.y, 0)))
+  //   c2.points.forEach((v2) => vertices.push(new THREE.Vector3(v2.x, v2.y, 0)))
 
-  c1.circleGeometry.vertices.map((vertex, index) => {
-    geometry.vertices.push(
-      new THREE.Vector3(vertex.x + c1.x, vertex.y + c1.y, vertex.z + c1.z)
-    )
-  })
-
-  c2.circleGeometry.vertices.map((vertex, index) => {
-    geometry.vertices.push(
-      new THREE.Vector3(vertex.x + c2.x, vertex.y + c2.y, vertex.z + c2.z)
-    )
-  })
-
-  console.log(geometry)
-
-  geometry.faces.push(new THREE.Face3(2, 7, 1))
-
-  const cylinder = new THREE.Mesh(geometry, material)
-  scene.add(cylinder)
-}
-
-addLine = (geometry, positions, color) => {
-  geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
-  drawCount = 2
-  geometry.setDrawRange(0, drawCount)
-
-  let material = new THREE.LineBasicMaterial({ color })
-  line = new THREE.Line(geometry, material)
-
-  scene.add(line)
+  const geometry = new THREE.Geometry()
+  //   console.log(vertices)
 }
 
 updatePositions = () => {
@@ -137,7 +141,6 @@ onMouseMove = (event) => {
     mouse.y = -(event.clientY / height) * 2 + 1
     let currentPos = new THREE.Vector3(mouse.x, mouse.y, 0)
 
-    currentPos.unproject(camera)
     posArray.push(currentPos)
   }
 }
@@ -148,10 +151,13 @@ onMouseDown = (event) => {
   let z = 0
 
   let currentPos = new THREE.Vector3(x, y, z)
-  currentPos.unproject(camera)
   posArray.push(currentPos)
 
-  drawCircle(currentPos, 0.5, 4)
+  circle = new THREE.Shape()
+    .moveTo(0, 0)
+    .absarc(0, 0, 0.4, 0, Math.PI * 2, false)
+
+  addLineShape(circle, 0x0000ff, currentPos, { rx: 5, ry: 0, rz: 0 }, 1)
 
   canvas.addEventListener("mousemove", onMouseMove, false)
   canvas.addEventListener("mouseup", onMouseUp, false)
