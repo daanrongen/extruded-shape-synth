e1f067373023a96dc93a239fcfc365a556504efc
daanrongen <daanrongen@hotmail.com>
2021-01-14 22:03:53 +0000

    extrudepath on buffergeometry

--- final-project/script.js
+++ final-project/script.js
@@ -10,18 +10,20 @@
  *
  * draw line dynamically:
  * https://jsfiddle.net/wilt/a21ey9y6/
+ *
+ * THREE.ExtrudeBufferGeometry
+ * https://discourse.threejs.org/t/three-extrudebuffergeometry-how-to-extrude-according-to-the-given-path-point/8349
  */
 
 var canvas, renderer, camera, light, scene, group
 var width = window.innerWidth
 var height = window.innerHeight
 
-var circle
-var circles = []
-
 var mouse = new THREE.Vector3()
-var drawCount = 2
+var drawCount = 10
+
 var posArray = []
+var shapesArray = []
 
 var line
 
@@ -36,8 +38,8 @@ function main() {
   renderer.setSize(width, height)
   scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 100)
-  camera.position.set(0, 0, 5)
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)
+  camera.position.set(0, 0, 2)
   camera.up.set(0, 1, 0)
   camera.lookAt(0, 0, 0)
 
@@ -45,90 +47,68 @@ function main() {
   light.position.set(5, 5, 5)
   scene.add(light)
 
-  group = new THREE.Group()
-  scene.add(group)
-
-  addLine(0x0000ff)
+  addLine()
 
   requestAnimationFrame(render)
 }
 
-render = () => {
+render = (time) => {
+  time *= 0.001
   renderer.render(scene, camera)
 
-  drawCount = posArray.length
-  line.geometry.setDrawRange(0, drawCount)
-  line.geometry.attributes.position.needsUpdate = true
+  // shapesArray.forEach((shape) => {
+  //   shape.rotation.x = time
+  //   shape.rotation.y = time
+  // })
 
   updatePositions()
   requestAnimationFrame(render)
 }
 
-addShape = (shape, color, { x, y, z }, { rx, ry, rz }) => {
-  const geometry = new THREE.ShapeBufferGeometry(shape)
-
-  mesh = new THREE.Mesh(
-    geometry,
-    new THREE.MeshPhongMaterial({ color: color, wireframe: true })
-  )
-  mesh.position.set(x, y, z)
-  mesh.rotation.set(rx, ry, rz)
-  group.add(mesh)
-
-  addLineShape(shape, color, { x, y, z }, { rx, ry, rz })
-}
-
-addLineShape = (shape, color, { x, y, z }, { rx, ry, rz }) => {
-  const points = shape.getPoints()
-
-  const geometryPoints = new THREE.BufferGeometry().setFromPoints(points)
-  const line = new THREE.Line(
-    geometryPoints,
-    new THREE.LineBasicMaterial({ color: color })
-  )
-  line.position.set(x, y, z)
-  line.rotation.set(rx, ry, rz)
-
-  const particles = new THREE.Points(
-    geometryPoints,
-    new THREE.PointsMaterial({ color: color, size: 0.05 })
-  )
-  particles.position.set(x, y, z)
-  particles.rotation.set(rx, ry, rz)
-
-  circles.push({ points, x, y, z })
-  group.add(line, particles)
-}
-
-addLine = (color) => {
+addLine = () => {
   const geometry = new THREE.BufferGeometry()
   let positions = new Float32Array(drawCount * 3)
 
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
   geometry.setDrawRange(0, drawCount)
-
-  const material = new THREE.LineBasicMaterial({ color, linewidth: 1 })
+  const material = new THREE.LineBasicMaterial()
   line = new THREE.Line(geometry, material)
 
-  group.add(line)
+  scene.add(line)
 }
 
-drawCylinder = (c1, c2) => {
-  console.log(c1, c2)
-  let vertices = []
+drawShape = (index) => {
+  group = new THREE.Group()
 
-  //   c1.points.forEach((v2) => vertices.push(new THREE.Vector3(v2.x, v2.y, 0)))
-  //   c2.points.forEach((v2) => vertices.push(new THREE.Vector3(v2.x, v2.y, 0)))
+  let circle = new THREE.Shape()
+  circle.moveTo(0, 0).absarc(0, 0, 0.05, 0, Math.PI * 2, false)
 
-  const geometry = new THREE.Geometry()
-  //   console.log(vertices)
+  let path = new THREE.CatmullRomCurve3(posArray)
+  let step = path.points.length
+  let geometry = new THREE.ExtrudeBufferGeometry(circle, {
+    extrudePath: path,
+    curveSegments: 4,
+    steps: step,
+  })
+
+  let material = new THREE.MeshPhongMaterial({ color: null })
+
+  var mesh = new THREE.Mesh(geometry, material)
+  // group.add(mesh)
+  scene.add(mesh)
+
+  shapesArray[index] = 1
 }
 
 updatePositions = () => {
   let positions = line.geometry.attributes.position.array
   let index = 0
 
-  for (let i = 0; i < posArray.length; i++) {
+  drawCount = posArray.length
+  line.geometry.setDrawRange(0, drawCount)
+  line.geometry.attributes.position.needsUpdate = true
+
+  for (let i = 0; i < drawCount; i++) {
     positions[index++] = posArray[i].x
     positions[index++] = posArray[i].y
     positions[index++] = posArray[i].z
@@ -139,9 +119,13 @@ onMouseMove = (event) => {
   if (renderer) {
     mouse.x = (event.clientX / width) * 2 - 1
     mouse.y = -(event.clientY / height) * 2 + 1
-    let currentPos = new THREE.Vector3(mouse.x, mouse.y, 0)
 
+    let currentPos = new THREE.Vector3(mouse.x, mouse.y, 0)
     posArray.push(currentPos)
+
+    if (posArray.length > 2) {
+      drawShape(shapesArray.length)
+    }
   }
 }
 
@@ -151,23 +135,16 @@ onMouseDown = (event) => {
   let z = 0
 
   let currentPos = new THREE.Vector3(x, y, z)
+  posArray = new Array()
   posArray.push(currentPos)
-
-  circle = new THREE.Shape()
-    .moveTo(0, 0)
-    .absarc(0, 0, 0.4, 0, Math.PI * 2, false)
-
-  addLineShape(circle, 0x0000ff, currentPos, { rx: 5, ry: 0, rz: 0 }, 1)
 
   canvas.addEventListener("mousemove", onMouseMove, false)
   canvas.addEventListener("mouseup", onMouseUp, false)
 }
 
-onMouseUp = (event) => {
-  if (circles.length == 2) {
-    drawCylinder(circles[0], circles[1])
-  }
-
+onMouseUp = () => {
+  // shapesArray.push(posArray)
+  console.log(shapesArray)
   canvas.removeEventListener("mousemove", onMouseMove, false)
 }
 
